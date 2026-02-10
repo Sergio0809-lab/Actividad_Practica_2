302af9a7b4e3ab83b76e774a67ed8399583d08ce
sergioperez-dev <sp7134651@gmail.com>
2026-02-10 19:53:56 -0400

    Dia 1

---
+++ Actividad_Practica_2/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Actividad_Practica_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            ejercicio1ToolStripMenuItem = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem1 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem2 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem3 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem4 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem5 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem6 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem7 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem8 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem9 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem10 = new ToolStripMenuItem();
+            ejercicio1ToolStripMenuItem11 = new ToolStripMenuItem();
+            ejercicio12ToolStripMenuItem = new ToolStripMenuItem();
+            ejercicio13ToolStripMenuItem = new ToolStripMenuItem();
+            ejercicio14ToolStripMenuItem = new ToolStripMenuItem();
+            ejercicio15ToolStripMenuItem = new ToolStripMenuItem();
+            ejercicio16ToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { ejercicio1ToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // ejercicio1ToolStripMenuItem
+            // 
+            ejercicio1ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { ejercicio1ToolStripMenuItem1, ejercicio1ToolStripMenuItem2, ejercicio1ToolStripMenuItem3, ejercicio1ToolStripMenuItem4, ejercicio1ToolStripMenuItem5, ejercicio1ToolStripMenuItem6, ejercicio1ToolStripMenuItem7, ejercicio1ToolStripMenuItem8, ejercicio1ToolStripMenuItem9, ejercicio1ToolStripMenuItem10, ejercicio1ToolStripMenuItem11, ejercicio12ToolStripMenuItem, ejercicio13ToolStripMenuItem, ejercicio14ToolStripMenuItem, ejercicio15ToolStripMenuItem, ejercicio16ToolStripMenuItem });
+            ejercicio1ToolStripMenuItem.Name = "ejercicio1ToolStripMenuItem";
+            ejercicio1ToolStripMenuItem.Size = new Size(85, 24);
+            ejercicio1ToolStripMenuItem.Text = "Ejercicios";
+            // 
+            // ejercicio1ToolStripMenuItem1
+            // 
+            ejercicio1ToolStripMenuItem1.Name = "ejercicio1ToolStripMenuItem1";
+            ejercicio1ToolStripMenuItem1.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem1.Text = "Ejercicio 1";
+            // 
+            // ejercicio1ToolStripMenuItem2
+            // 
+            ejercicio1ToolStripMenuItem2.Name = "ejercicio1ToolStripMenuItem2";
+            ejercicio1ToolStripMenuItem2.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem2.Text = "Ejercicio 2";
+            ejercicio1ToolStripMenuItem2.Click += ejercicio1ToolStripMenuItem2_Click;
+            // 
+            // ejercicio1ToolStripMenuItem3
+            // 
+            ejercicio1ToolStripMenuItem3.Name = "ejercicio1ToolStripMenuItem3";
+            ejercicio1ToolStripMenuItem3.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem3.Text = "Ejercicio 3";
+            // 
+            // ejercicio1ToolStripMenuItem4
+            // 
+            ejercicio1ToolStripMenuItem4.Name = "ejercicio1ToolStripMenuItem4";
+            ejercicio1ToolStripMenuItem4.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem4.Text = "Ejercicio 4";
+            // 
+            // ejercicio1ToolStripMenuItem5
+            // 
+            ejercicio1ToolStripMenuItem5.Name = "ejercicio1ToolStripMenuItem5";
+            ejercicio1ToolStripMenuItem5.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem5.Text = "Ejercicio 5";
+            // 
+            // ejercicio1ToolStripMenuItem6
+            // 
+            ejercicio1ToolStripMenuItem6.Name = "ejercicio1ToolStripMenuItem6";
+            ejercicio1ToolStripMenuItem6.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem6.Text = "Ejercicio 6";
+            // 
+            // ejercicio1ToolStripMenuItem7
+            // 
+            ejercicio1ToolStripMenuItem7.Name = "ejercicio1ToolStripMenuItem7";
+            ejercicio1ToolStripMenuItem7.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem7.Text = "Ejercicio 7";
+            // 
+            // ejercicio1ToolStripMenuItem8
+            // 
+            ejercicio1ToolStripMenuItem8.Name = "ejercicio1ToolStripMenuItem8";
+            ejercicio1ToolStripMenuItem8.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem8.Text = "Ejercicio 8";
+            // 
+            // ejercicio1ToolStripMenuItem9
+            // 
+            ejercicio1ToolStripMenuItem9.Name = "ejercicio1ToolStripMenuItem9";
+            ejercicio1ToolStripMenuItem9.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem9.Text = "Ejercicio 9";
+            // 
+            // ejercicio1ToolStripMenuItem10
+            // 
+            ejercicio1ToolStripMenuItem10.Name = "ejercicio1ToolStripMenuItem10";
+            ejercicio1ToolStripMenuItem10.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem10.Text = "Ejercicio 10";
+            // 
+            // ejercicio1ToolStripMenuItem11
+            // 
+            ejercicio1ToolStripMenuItem11.Name = "ejercicio1ToolStripMenuItem11";
+            ejercicio1ToolStripMenuItem11.Size = new Size(224, 26);
+            ejercicio1ToolStripMenuItem11.Text = "Ejercicio 11";
+            // 
+            // ejercicio12ToolStripMenuItem
+            // 
+            ejercicio12ToolStripMenuItem.Name = "ejercicio12ToolStripMenuItem";
+            ejercicio12ToolStripMenuItem.Size = new Size(224, 26);
+            ejercicio12ToolStripMenuItem.Text = "Ejercicio 12";
+            // 
+            // ejercicio13ToolStripMenuItem
+            // 
+            ejercicio13ToolStripMenuItem.Name = "ejercicio13ToolStripMenuItem";
+            ejercicio13ToolStripMenuItem.Size = new Size(224, 26);
+            ejercicio13ToolStripMenuItem.Text = "Ejercicio 13";
+            // 
+            // ejercicio14ToolStripMenuItem
+            // 
+            ejercicio14ToolStripMenuItem.Name = "ejercicio14ToolStripMenuItem";
+            ejercicio14ToolStripMenuItem.Size = new Size(224, 26);
+            ejercicio14ToolStripMenuItem.Text = "Ejercicio 14";
+            // 
+            // ejercicio15ToolStripMenuItem
+            // 
+            ejercicio15ToolStripMenuItem.Name = "ejercicio15ToolStripMenuItem";
+            ejercicio15ToolStripMenuItem.Size = new Size(224, 26);
+            ejercicio15ToolStripMenuItem.Text = "Ejercicio 15";
+            // 
+            // ejercicio16ToolStripMenuItem
+            // 
+            ejercicio16ToolStripMenuItem.Name = "ejercicio16ToolStripMenuItem";
+            ejercicio16ToolStripMenuItem.Size = new Size(224, 26);
+            ejercicio16ToolStripMenuItem.Text = "Ejercicio 16";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Acticidad practica 2";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem1;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem2;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem3;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem4;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem5;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem6;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem7;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem8;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem9;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem10;
+        private ToolStripMenuItem ejercicio1ToolStripMenuItem11;
+        private ToolStripMenuItem ejercicio12ToolStripMenuItem;
+        private ToolStripMenuItem ejercicio13ToolStripMenuItem;
+        private ToolStripMenuItem ejercicio14ToolStripMenuItem;
+        private ToolStripMenuItem ejercicio15ToolStripMenuItem;
+        private ToolStripMenuItem ejercicio16ToolStripMenuItem;
+    }
+}
